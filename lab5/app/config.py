9cfdcf8f34805963ebfcc80a76a28e9dcee0f572
brothel2077 <90911100+brothel2077@users.noreply.github.com>
2023-10-28 18:24:23 +0300

    Delete lab5 directory

--- lab5/app/config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'e341e6698cb20dd889d040a9be7d5fc129cb06255f349bd6ea3f901afe8d61b4'
-
-MYSQL_USER = 'std_1909_lab4'
-MYSQL_PASSWORD = 'denis2017'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_1909_lab4'
-
-ADMIN_ROLE_ID = 1
--- lab5/app/static/delete.js
+++
@@ -1,9 +0,0 @@
-
-const deleteModalEl = document.getElementById('deleteModal');
-deleteModalEl.addEventListener('show.bs.modal', function (event) {
-    let url = event.relatedTarget.dataset.url;
-    let form = this.querySelector('form');
-    form.action = url; 
-    let span = this.querySelector('.delete-user-name');
-    span.textContent = event.relatedTarget.dataset.login;
-});
